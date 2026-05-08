92ec4374ef5533c008e80b25553ffd55e1f57125
mawmawia <mawia.steve094@gmail.com>
2026-05-08 14:04:17 +0530

    Update checker.py

--- checker.py
+++ checker.py
@@ -211,5 +211,7 @@ def stats():
         risky = total - safe
     return jsonify({"total": total, "safe": safe, "risky": risky})
 
+import os
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
